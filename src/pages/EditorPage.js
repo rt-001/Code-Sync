5f376ea2825140b7816306251ea43f180314e56b
Romit Tiwari <romittiwari100@gmail.com>
2022-12-08 17:42:45 +0530

    first commit

---
+++ src/pages/EditorPage.js
@@ -0,0 +1,11 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const EditorPage = () => {
+  return (
+    <div>
+      <p>hello EditorPage</p>
+    </div>
+  );
+};
+
+export default EditorPage;
